6a6c28b48877b814e60514be3fce346e313c8118
anwar439 <anwar@smpia9.sch.id>
2026-07-19 02:09:17 +0700

    build: add entry point for Node.js production server
    Configure app.js to execute the compiled server file for compatibility with cPanel Node.js Selector.

---
+++ app.js
@@ -0,0 +1,3 @@
+// Ini adalah file entry point untuk cPanel Node.js Selector.
+// File ini akan memanggil hasil build production server kita yang berada di dist/server.cjs.
+require('./dist/server.cjs');
